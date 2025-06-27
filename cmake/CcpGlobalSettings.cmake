13eaf99d398355aa79b9a0663f132a3a6aa1b194
ccp-serpent <162123512+ccp-serpent@users.noreply.github.com>
2025-06-27 13:07:02 +0000

    Control ´CMAKE_OSX_ARCHITECTURES´ from preset

--- cmake/CcpGlobalSettings.cmake
+++ cmake/CcpGlobalSettings.cmake
@@ -3,7 +3,6 @@ if(APPLE)
     # we are building on, but we want to stick to our policy of supporting the last three releases.
     set(CMAKE_OSX_DEPLOYMENT_TARGET 10.14 CACHE STRING "The minimum macOS version we target." FORCE)
     message(STATUS "Building for minimum macOS version: ${CMAKE_OSX_DEPLOYMENT_TARGET}")
-    set(CMAKE_OSX_ARCHITECTURES "arm64;x86_64")
     message(STATUS "Building for ${CMAKE_OSX_ARCHITECTURES} architecture")
 endif()
 
